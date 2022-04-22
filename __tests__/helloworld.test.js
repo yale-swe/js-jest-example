580ad5728738404adb3bcf4ce91ee20754453762
wu-json <jason.wu2153@gmail.com>
2022-04-22 17:39:34 -0400

    test: better expect

--- __tests__/helloworld.test.js
+++ __tests__/helloworld.test.js
@@ -4,7 +4,9 @@ import request from "supertest";
 describe("JS Jest Example Tests", () => {
   it("should send the right payload", async () => {
     const res = await request(app).get("/hello");
-    console.log(res);
     expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      message: "Hello world!",
+    });
   });
 });
